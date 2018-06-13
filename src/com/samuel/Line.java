b4db607ed224fbe5373dde2c1e39f908fb4457d2
Garrett Hart <garrett7621@gmail.com>
2018-06-12 23:24:36 -0400

    distances save to file, mapping works!

--- src/com/samuel/Line.java
+++ src/com/samuel/Line.java
@@ -8,12 +8,16 @@ import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
 public class Line {
 	public HvlCoord2D start;
 	public HvlCoord2D end;
+	public float distance;
 
 	public Line(HvlCoord2D startPt, HvlCoord2D endPt) {
 		this.start = startPt;
 		this.end = endPt;
 	}
 	public void draw(float delta) {
-		HvlPainter2D.hvlDrawLine(start.x, start.y, end.x, end.y, Color.white);
+		HvlPainter2D.hvlDrawLine(this.start.x, this.start.y, this.end.x, this.end.y, Color.white);
+	}
+	public void setDistance(float disArg) {
+		this.distance = disArg;
 	}
 }
--- src/com/samuel/Main.java
+++ src/com/samuel/Main.java
@@ -1,16 +1,25 @@
 package com.samuel;
 
-import org.newdawn.slick.Color;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
+import org.newdawn.slick.Color;
 
 import com.osreboot.ridhvl.HvlCoord2D;
 import com.osreboot.ridhvl.HvlMath;
 import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
+import com.osreboot.ridhvl.menu.HvlMenu;
+import com.osreboot.ridhvl.menu.component.HvlArrangerBox;
+import com.osreboot.ridhvl.menu.component.HvlArrangerBox.ArrangementStyle;
+import com.osreboot.ridhvl.menu.component.HvlComponentDrawable;
+import com.osreboot.ridhvl.menu.component.HvlSpacer;
+import com.osreboot.ridhvl.menu.component.HvlTextBox;
+import com.osreboot.ridhvl.painter.painter2d.HvlFontPainter2D;
 import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
 import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
 
@@ -21,58 +30,119 @@ public class Main extends HvlTemplateInteg2D{
 	public Main(){
 		super(60, 720, 680, "Motion Mapper", new HvlDisplayModeDefault());
 	}
-	
+	public void save(String fileName) {
+		fileName = UI.getChildOfType(HvlArrangerBox.class, 0).getFirstOfType(HvlTextBox.class).getText(); //gets text box input; saves as variable
+		 
+		String userHomeFolder = System.getProperty("user.home")+"/Documents"; 
+		File profile = new File(userHomeFolder, fileName+".txt");  
+		File loader = new File(userHomeFolder, fileName+"Loader.BOND");//creates new txt file with inputed name
+		BufferedWriter output = null;
+		
+		try {
+			
+			output = new BufferedWriter(new FileWriter(profile));  
+			for(int i = 1; i < points.size(); i++) {
+				output.write("Distance: "+lines.get(i).distance);
+				output.newLine();
+			}
+		
+			output.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	float mouseX;
 	float mouseY;
 	float drawTimer;
 	float pointY;
 	boolean generatedPath;
+	static HvlFontPainter2D gameFont;
+
 	ArrayList<Line> lines;
 	ArrayList<Point> points;
-	
+	HvlMenu UI;
+	String file;
 	@Override
 	public void initialize() {
+		getTextureLoader().loadResource("osfont");
+		gameFont =  new HvlFontPainter2D(getTexture(0), HvlFontPainter2D.Preset.FP_INOFFICIAL,.5f,8f,0); //font definition
+
 		mouseX = Mouse.getX();
 		mouseY = Mouse.getY();
 		drawTimer = 50;
 		generatedPath = false;
+		file = "";
 		lines = new ArrayList<Line>();
 		points = new ArrayList<Point>();
+		UI = new HvlMenu() {
+			public void draw(float delta) {
+				mouseX = Mouse.getX();
+				mouseY = -Mouse.getY() + Display.getHeight();
+				drawTimer-=delta * 750;
+				gameFont.drawWordc("Name your \n file and then \n press \nL _ SHIFT", 650, 100, Color.white, 0.35f);
+				HvlPainter2D.hvlDrawQuad(520, 0, 200, 680, new Color(130, 130, 130, 0.3f));
+				if(drawTimer <=0 && Mouse.isButtonDown(0) && Mouse.getX() < 520) {
+					if(lines.size() == 0) {
+						Line line = new Line(new HvlCoord2D(mouseX, mouseY), new HvlCoord2D(mouseX, mouseY));
+						lines.add(line);
+						drawTimer = 50;
+					}
+					if(lines.size() > 0) {
+						Line line = new Line(lines.get(lines.size()-1).end, new HvlCoord2D(mouseX, mouseY));
+						lines.add(line);
+						drawTimer = 50;
+
+					}
+				}
+				for(Line lineWave : lines) {
+					lineWave.draw(delta);
+				}
+				if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) && generatedPath == false) {
+					for(float i = 0; i < lines.size(); i++) {
+						if(lines.size() > 1 && i < lines.size()-1) {
+							
+							lines.get((int)i).setDistance(HvlMath.distance(lines.get((int)i).end, lines.get((int)i+1).end));
+						}
+						pointY = HvlMath.map(lines.get((int)i).end.x, lines.get((int) i).start.x, lines.get((int) i).end.x, lines.get((int) i).start.y, lines.get((int) i).end.y);
+						
+						Point point = new Point(lines.get((int)i).end.x, pointY);
+						points.add(point);
+					}
+					file = UI.getChildOfType(HvlArrangerBox.class, 0).getFirstOfType(HvlTextBox.class).getText(); //gets text box input; saves as variable
+
+					save(file);
+					generatedPath = true;
+				}
+				for(Point pointWave : points) {
+					pointWave.draw(delta);
+				}
+				super.draw(delta);
+			}
+		};	
+		UI.add(new HvlArrangerBox.Builder().setStyle(ArrangementStyle.HORIZONTAL).setWidth(250).setHeight(100).setX(Display.getWidth() - 235).setY(Display.getHeight()-180).build());
+
+		UI.getChildOfType(HvlArrangerBox.class, 0).add(new HvlSpacer(30, 30));
+		UI.getChildOfType(HvlArrangerBox.class, 0).add(new HvlTextBox.Builder().setWidth(200).setHeight(50).setFont(gameFont).setTextColor(Color.darkGray).setTextScale(0.25f).setOffsetY(20).setOffsetX(20).setText("").setFocusedDrawable(new HvlComponentDrawable() {	
+			@Override
+			public void draw(float delta, float x, float y, float width, float height) {
+				HvlPainter2D.hvlDrawQuad(x,y,width,height, Color.lightGray);	
+			}
+		}).setUnfocusedDrawable(new HvlComponentDrawable() {
+			
+			@Override
+			public void draw(float delta, float x, float y, float width, float height) {
+				HvlPainter2D.hvlDrawQuad(x,y,width,height, Color.green);	
+			}
+		}).build());
+		HvlMenu.setCurrent(UI);
 	}
 	@Override
 	public void update(float delta) {
-		mouseX = Mouse.getX();
-		mouseY = -Mouse.getY() + Display.getHeight();
-		drawTimer-=delta * 10000;
-		if(drawTimer <=0 && Mouse.isButtonDown(0)) {
-			if(lines.size() == 0) {
-				Line line = new Line(new HvlCoord2D(mouseX, mouseY), new HvlCoord2D(mouseX, mouseY));
-				lines.add(line);
-				drawTimer = 50;
-			}
-			if(lines.size() > 0) {
-				Line line = new Line(lines.get(lines.size()-1).end, new HvlCoord2D(mouseX, mouseY));
-				lines.add(line);
-				drawTimer = 50;
 
-			}
+		HvlMenu.updateMenus(delta);
+		if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+			exit();
 		}
-		for(Line lineWave : lines) {
-			lineWave.draw(delta);
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_G) && generatedPath == false) {
-			for(float i = 0; i < lines.size()-1; i++) {
-			
-				pointY = HvlMath.map(i, lines.get((int) i).start.x, lines.get((int) i).end.x, lines.get((int) i).start.y, lines.get((int) i).end.y);
-				Point point = new Point(lines.get((int)i).end.x, pointY);
-				points.add(point);
-			}
-		
-			generatedPath = true;
-		}
-		for(Point pointWave : points) {
-			pointWave.draw(delta);
-		}
-		
 	}
 }
