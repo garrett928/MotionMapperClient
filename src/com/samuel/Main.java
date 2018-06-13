593efbe7102abc4e5898886e88b18fb2b2612fd8
Garrett Hart <garrett7621@gmail.com>
2018-06-12 22:21:29 -0400

    Added points (kinda lol)
    yeeeeeeeeeeeeeet

--- src/com/samuel/Main.java
+++ src/com/samuel/Main.java
@@ -2,9 +2,14 @@ package com.samuel;
 
 import org.newdawn.slick.Color;
 
+import java.util.ArrayList;
+
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 
+import com.osreboot.ridhvl.HvlCoord2D;
+import com.osreboot.ridhvl.HvlMath;
 import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
 import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
 import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
@@ -20,18 +25,54 @@ public class Main extends HvlTemplateInteg2D{
 	float mouseX;
 	float mouseY;
 	float drawTimer;
+	float pointY;
+	boolean generatedPath;
+	ArrayList<Line> lines;
+	ArrayList<Point> points;
 	
 	@Override
 	public void initialize() {
 		mouseX = Mouse.getX();
 		mouseY = Mouse.getY();
 		drawTimer = 50;
+		generatedPath = false;
+		lines = new ArrayList<Line>();
+		points = new ArrayList<Point>();
 	}
 	@Override
 	public void update(float delta) {
 		mouseX = Mouse.getX();
 		mouseY = -Mouse.getY() + Display.getHeight();
+		drawTimer-=delta * 10000;
+		if(drawTimer <=0 && Mouse.isButtonDown(0)) {
+			if(lines.size() == 0) {
+				Line line = new Line(new HvlCoord2D(mouseX, mouseY), new HvlCoord2D(mouseX, mouseY));
+				lines.add(line);
+				drawTimer = 50;
+			}
+			if(lines.size() > 0) {
+				Line line = new Line(lines.get(lines.size()-1).end, new HvlCoord2D(mouseX, mouseY));
+				lines.add(line);
+				drawTimer = 50;
+
+			}
+		}
+		for(Line lineWave : lines) {
+			lineWave.draw(delta);
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_G) && generatedPath == false) {
+			for(float i = 0; i < lines.size()-1; i++) {
+			
+				pointY = HvlMath.map(i, lines.get((int) i).start.x, lines.get((int) i).end.x, lines.get((int) i).start.y, lines.get((int) i).end.y);
+				Point point = new Point(lines.get((int)i).end.x, pointY);
+				points.add(point);
+			}
 		
-		HvlPainter2D.hvlDrawQuadc(mouseX, mouseY, 50, 50, Color.blue);
+			generatedPath = true;
+		}
+		for(Point pointWave : points) {
+			pointWave.draw(delta);
+		}
+		
 	}
 }
--- src/com/samuel/Point.java
+++ src/com/samuel/Point.java
@@ -1,5 +1,18 @@
 package com.samuel;
 
-public class Point {
+import org.newdawn.slick.Color;
 
+import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
+
+public class Point {
+	public float xPos;
+	public float yPos;
+	
+	public Point(float xArg, float yArg) {
+		this.xPos = xArg;
+		this.yPos = yArg;
+	}
+	public void draw(float delta) {
+		HvlPainter2D.hvlDrawQuadc(this.xPos, this.yPos, 10, 10, Color.blue);
+	}
 }
